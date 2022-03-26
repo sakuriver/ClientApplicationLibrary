3ae08dbd496e3f083629a8e104f90229f904c3b6
sakuriver <suzuerain@yahoo.co.jp>
2022-03-26 23:01:08 +0900

    プロジェクト ファイルを追加します。

---
+++ ClientApplicationLibrary/Network/HttpConnection.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace ClientApplicationLibrary.Network
+{
+    /// <summary>
+    /// http通信の管理クラス
+    /// babylon.jsや各種C#用に共通利用での呼び出しとして利用
+    /// </summary>
+    public class HttpConnectionManager
+    {
+
+        private string _host;
+
+        private NameValueCollection _querys;
+
+        private HttpClient _client;
+
+        public void setParameter(string host, NameValueCollection querys)
+        {
+            this._host = host;
+            this._querys = querys;
+        }
+
+        /// <summary>
+        /// get通信を実行
+        /// </summary>
+        /// <param name="path"></param>
+        public async Task<string> GetRequest(string path)
+        {
+
+            var url = $"{_host}{path}?";
+            var count = 0;
+            foreach (var key in _querys.AllKeys)
+            {
+                url += $"{key}={_querys[key]}";
+                count++;
+                if (count < _querys.Count - 1) {
+                    url += "&";
+                }
+            }
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+            _client = new HttpClient();
+            var result = await _client.SendAsync(request);
+            var resultString = await result.Content.ReadAsStringAsync();
+            Console.WriteLine(resultString);
+
+            return resultString;
+        }
+
+        /// <summary>
+        /// get通信を実行
+        /// </summary>
+        /// <param name="path"></param>
+        public async Task<string> PostRequest(string path, Dictionary<string, object> param)
+        {
+
+            var url = $"{_host}{path}";
+
+            _client = new HttpClient();
+
+            var jsonString = System.Text.Json.JsonSerializer.Serialize(param);
+            var content = new StringContent(jsonString, Encoding.UTF8, @"application/json");
+            content.Headers.Add("DNT", "1");
+            var result = await _client.PostAsync(url, content);
+            var resultString = await result.Content.ReadAsStringAsync();
+            Console.WriteLine(resultString);
+
+            return resultString;
+        }
+
+    }
+}
---
+++ ClientLibraryConsoleSample/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ClientApplicationLibrary.Network;
+using System.Collections.Specialized;
+
+// 各種.net6向け用として、用意
+HttpConnectionManager HttpConnection = new HttpConnectionManager();
+var host = "https://contentsroomql.azurewebsites.net";
+// https://dgraph.io/docs/graphql/api/requests/#:~:text=GraphQL%20requests%20can%20be%20sent%20via%20HTTP%20POST,is%20a%20valid%20POST%20body%20for%20a%20query%3A 
+
+
+// お部屋情報の取得を開始します
+
+NameValueCollection collection = new NameValueCollection();
+collection["query"] = "{room(id: 1){id name root{ pictures{ url name position{ x y z}}}}}";
+collection["variables"] = "{}";
+collection["operationName"] = "{}";
+HttpConnection.setParameter(host, collection);
+// graphqlのquery情報をPOSTにつめて実行する
+var param = new Dictionary<string, object>()
+{
+    ["operationName"] = null,
+    ["variables"] = "{}",
+    ["query"] = "{room(id: 1) {id name root { pictures { url name position { x y z} } }}}",
+};
+      var graphqlResult = await HttpConnection.PostRequest("/api/graphql", param);
+Console.WriteLine($"Hello, World! api result {graphqlResult}");
